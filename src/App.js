bd1fa9fddd2673e674688fb997db57952363819f
MisterC1ean <robert.keller44@icloud.com>
2020-05-22 15:07:48 -0400

    Working on Component basics

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { Route, Link } from "react-router-dom"
+
 
 import './App.css';
-import GameContainer from './Containers/GameContainer';
-import HighScoreContainer from './Containers/HighScoreContainer';
-import HowToPlayContainer from './Containers/HowToPlayContainer';
-import Container from '@material-ui/core/Container'
-import Box from '@material-ui/core/Box'
 
 
 
 
 function App() {
   return (
-    <Router>
-        <Box>
-        <Container maxWidth="sm">
-        <nav>
-          <ul className='menu'>
-            <li>
-              <Link to="/game">Start</Link>
-              <Route path="/game" component={GameContainer} />
-            </li>
-            <li>
-              <Link to="/high_scores">High Scores</Link>
-              <Route path="/high_scores" component={HighScoreContainer} />
-            </li>
-            <li>
-              <Link to="/how_to_play">How To Play</Link>
-              <Route path="/how_to_play" component={HowToPlayContainer} />
-            </li>
-          </ul>
-        </nav>
-        </Container>
-        </Box>
-    </Router>
+
   );
 }
 
--- src/Containers/GameContainer.js
+++ src/Containers/GameContainer.js
@@ -1,21 +1,8 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
 
 function GameContainer() {
-  const useStyles = makeStyles({
-    root: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      border: 0,
-      borderRadius: 3,
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-      color: 'white',
-      height: 48,
-      padding: '0 30px',
-    },
-  });
-  const classes = useStyles()
   return(
-    <h1 className={classes.root}>This is the Game Page</h1>
+    <h1>This is the Game Page</h1>
   )
 }
 
