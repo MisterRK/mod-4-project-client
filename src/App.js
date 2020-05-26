c3b3ae75e6e8a10f4e5a60654e58fc5d16b4c707
Asilver-jpg <asilver7493@gmail.com>
2020-05-26 09:58:48 -0400

    some merge errors fixed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from 'react'
 import { Route } from "react-router-dom"
 import HowToPlay from './components/HowToPlay'
-import GameContainer from './container/GameContainer'
 import Home from './components/Home'
 import PlayersContainer from './containers/PlayersContainer'
 import ScoresContainer from './containers/ScoresContainer'
 import './App.css';
-import Scene from "./components/Scene.js"
-import GameContainer from "./components/GameContainer.js"
+import GameContainer from "./containers/GameContainer.js"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <GameContainer></GameContainer>
-      </header>
-    </div>
-  );
-=======
-=======
-
->>>>>>> 7f50b1f14b1e2668a97260a12ccbab7630473bf9
 class App extends React.Component {
 
   render(){
@@ -36,11 +20,6 @@ class App extends React.Component {
       </div>
     );
   }
-<<<<<<< HEAD
->>>>>>> 50eb88872b7d9a4819a02977f3304603e3bdc85e
-=======
-
->>>>>>> 7f50b1f14b1e2668a97260a12ccbab7630473bf9
 }
 
 export default App;
